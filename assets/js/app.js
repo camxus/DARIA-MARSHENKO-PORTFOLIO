50808069cf58b5706f0cf32640a90b6de02b0d91
cam <camillus.konkwo@gmail.com>
2021-09-09 16:12:57 +0200

    lot of changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,12 +10,12 @@ const text2 = intro.querySelector('h1');
 
 const controller = new ScrollMagic.Controller();
 
-let scene = new ScrollMagic.Scene({
-    duration: 2000,
-    triggerElement: intro,
-    triggerHook: 0,
-})
-    .addTo(controller);
+// let scene = new ScrollMagic.Scene({
+//     duration: 2000,
+//     triggerElement: intro,
+//     triggerHook: 0,
+// })
+//     .addTo(controller);
 
 
 
@@ -29,77 +29,10 @@ let scene = new ScrollMagic.Scene({
         triggerElement: intro,
         triggerHook: 0,
     })
-    // .setPin(intro)
+    // .setPin(text2)
     .setTween(text2Anim)
     .addTo(controller)
 
-    //viewphotos
-    //TEXT ANIMATION
-    const text3Anim = TweenMax.fromTo(text3, 3, { opacity: 0 }, { opacity: 1 });
-    
-    let scene4 = new ScrollMagic.Scene({
-        duration: 3000,
-        triggerElement: intro,
-        triggerHook: 0,
-    })
-    // .setPin(intro)
-    .setTween(text3Anim)
-    .addTo(controller)
-    
-    //gotovideo
-    //TEXT ANIMATION
-    const text1Anim = TweenMax.fromTo(text1, 3, { opacity: 0 }, { opacity: 1 });
-    
-    let scene2 = new ScrollMagic.Scene({
-        duration: 3000,
-        triggerElement: intro,
-        triggerHook: 0,
-    })
-    // .addIndicators(controller)
-    .setTween(text1Anim)
-    .addTo(controller)
-
-
-    const linksAnim = TweenMax.fromTo(links, 3, { opacity: 0 }, { opacity: 1 });
-    
-    let scene5 = new ScrollMagic.Scene({
-        duration: 3000,
-        triggerElement: intro,
-        triggerHook: 0,
-    })
-    // .addIndicators(controller)
-    .setPin(intro)
-    .setTween(linksAnim)
-    .addTo(controller)
-
-    const lineAnim = TweenMax.fromTo(line, 3, { height: "0%" }, { height: "100%" });
-    
-    let scene6= new ScrollMagic.Scene({
-        duration: 3000,
-        triggerElement: intro,
-        triggerHook: 0,
-    })
-    // .addIndicators(controller)
-    .setTween(lineAnim)
-    .addTo(controller)
-
-    const lineAnim2 = TweenMax.fromTo(line, 3, { opacity: "0" }, { opacity: "1" });
-    
-    let scene7= new ScrollMagic.Scene({
-        duration: 3000,
-        triggerElement: intro,
-        triggerHook: 0,
-    })
-    // .addIndicators(controller)
-    .setTween(lineAnim2)
-    .addTo(controller)
-    
-    
-//ONLOAD PAUSE
-// function loadFrame() {
-//     window.scroll(0, 5000)
-// };
-// window.onload = setTimeout(loadFrame, 3500);
 
 if (history.scrollRestoration) {
     history.scrollRestoration = 'manual';
