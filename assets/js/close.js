218ccffe61118c1dd256854bf88f0350c92e45ae
cam <camillus.konkwo@gmail.com>
2021-10-19 18:23:30 +0200

    none

--- assets/js/close.js
+++ assets/js/close.js
@@ -1,42 +1,58 @@
-const menuBtn = document.querySelector('.close-btn');
-const area = document.querySelector('.close-area');
-const videoBtn = document.querySelector('.video_area');
-const paintingsBtn = document.querySelector('.preview');
+const menuBtn = document.querySelectorAll('.close-btn');
+const area = document.querySelectorAll('.close-area');
+const videoBtn = document.querySelectorAll('.video_area');
+const paintingsBtn = document.querySelectorAll('.preview');
 
-let menuOpen = false;
-area.addEventListener('click', () => {
-  if(!menuOpen) {
-    menuBtn.classList.add('open');
-    menuOpen = true;
-  } else {
-    menuBtn.classList.remove('open');
-    menuOpen = false;
-  }
-});
+const menuOpen = false;
+// area.forEach(function(item) {
+//     item.addEventListener('click', function() {
+//         console.log('run')
 
-if (videoBtn !== null)
-{
-videoBtn.addEventListener('click', () => {
-    if(!menuOpen) {
-      menuBtn.classList.add('open');
-      menuOpen = true;
-    } else {
-      menuBtn.classList.remove('open');
-      menuOpen = false;
-    }
-  });
-}
+//     if(!menuOpen) {
+//         menuBtn.classList.add('open');
+//         menuOpen = true;
+//     } else {
+//         menuBtn.classList.remove('open');
+//         menuOpen = false;
+//     }
+//     })
+// })
+// ;
 
-if (paintingsBtn !== null)
-{
-paintingsBtn.addEventListener('click', () => {
-    console.log("daria")
-    if(!menuOpen) {
-      menuBtn.classList.add('open');
-      menuOpen = true;
-    } else {
-        menuBtn.classList.remove('open');
-        menuOpen = false;
-    }
-  });
-}
+// if (videoBtn !== null)
+// {
+//     videoBtn.forEach(function(item) {
+//         item.addEventListener('click', function() {
+//             console.log(menuOpen)
+//         if(!menuOpen) {
+//         menuBtn.classList.add('open');
+//         menuOpen = true;
+//         } else {
+//         menuBtn.classList.remove('open');
+//         menuOpen = false;
+//         }
+//     })
+//   })
+// }
+
+// if (paintingsBtn !== null)
+// {
+//     paintingsBtn.forEach(function(item) {
+//         item.addEventListener('click', function() {
+//         console.log("daria")
+//         if(!menuOpen) {
+//         menuBtn.classList.add('open');
+//         menuOpen = true;
+//         } else {
+//             menuBtn.classList.remove('open');
+//             menuOpen = false;
+//         }
+//     })
+//   })
+// }
+
+
+// function closebtn()
+// {
+//     console.log(menuBtn)
+// }
